9396c8a0d659edbc73c447bcf1865bd3e3b12905
bleurubin <bleurubin@gmail.com>
2016-12-27 11:59:27 -0800

    use DISCRIMINATOR

--- grails-app/domain/multitenantsample/Foo.groovy
+++ grails-app/domain/multitenantsample/Foo.groovy
@@ -1,11 +1,13 @@
 package multitenantsample
 
 import grails.gorm.MultiTenant
+import grails.gorm.multitenancy.Tenants
 import grails.rest.Resource
 
 @Resource(uri='/foos')
 class Foo implements MultiTenant<Foo> {
 
+    String tenantId = Tenants.currentId()
     String name
 
     static constraints = {
